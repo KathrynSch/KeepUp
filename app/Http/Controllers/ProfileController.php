1b022d26caf7b6d731598617a4dca9303d4e71b5
Sanson Pauline <pauline.sanson10@gmail.com>
2017-04-19 08:34:31 +0200

    correction edit profile

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -55,7 +55,7 @@ class ProfileController extends Controller
         }
 		
 	$user->save();
-	return redirect()->route('profile',["id" => $user->id]);
+	return redirect()->route('about',["id" => $user->id]);
 
    	}
 
